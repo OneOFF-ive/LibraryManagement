4b2277e8bd52b693ca2d35862148e451f6940aa5
Wu <2321141691@qq.com>
2023-02-23 16:45:34 +0800

    Initial commit

---
+++ src/main/java/com/five/Entry.java
@@ -0,0 +1,88 @@
+package com.five;
+
+import org.apache.commons.cli.*;
+
+import java.util.Arrays;
+
+
+public class Entry {
+    public static void main(String[] args) throws ParseException {
+        Option[] options = {defineAddOpt(), defineDelOpt(), defineSeekOpt(), defineRentOpt(), defineReturnOpt(), defineHelpOpt()};
+
+        MyCli myCli = MyCli.getInstance();
+
+        for (var option:options) {
+            myCli.addOption(option, (values)->{
+                System.out.println(Arrays.toString(values));
+                return null;
+            });
+        }
+
+        myCli.parseCommandLine(args);
+    }
+
+    void registerFunction(Option[] options, MyCli myCli) {
+        myCli.addOption(options[0], (values) -> {
+            System.out.println(Arrays.toString(values));
+            return null;
+        });
+    }
+
+    public static void defineOptions() {
+        Option addBookOpt = defineAddOpt();
+        Option delBookOpt = defineDelOpt();
+        Option seekBookOpt = defineSeekOpt();
+        Option rentBookOpt = defineRentOpt();
+        Option returnBookOpt = defineReturnOpt();
+        Option helpOpt = defineHelpOpt();
+    }
+
+    public static Option defineAddOpt() {
+        return Option.builder("a")
+                .longOpt("addBook")
+                .argName("bookName")
+                .hasArgs()
+                .desc("add a book for library")
+                .build();
+    }
+
+    public static Option defineDelOpt() {
+         return Option.builder("d")
+                .longOpt("delBook")
+                .argName("bookName")
+                .hasArg()
+                .desc("del a book from library")
+                .build();
+    }
+
+    public static Option defineSeekOpt() {
+         return Option.builder("s")
+                .longOpt("seekBook")
+                .argName("bookName")
+                .hasArg()
+                .desc("Seek a book from library")
+                .build();
+    }
+
+    public static Option defineRentOpt() {
+         return Option.builder("r")
+                .longOpt("rentBook")
+                .argName("bookName")
+                .hasArg()
+                .desc("Rent a book from library")
+                .build();
+    }
+
+    public static Option defineReturnOpt() {
+        return Option.builder("rt")
+                .longOpt("ReturnBook")
+                .argName("bookName")
+                .hasArg()
+                .desc("Return a book for library")
+                .build();
+    }
+
+    public static Option defineHelpOpt() {
+        return new Option("h", "help", false, "print help message");
+    }
+}
---
+++ src/main/java/com/five/MyCli.java
@@ -0,0 +1,46 @@
+package com.five;
+
+import org.apache.commons.cli.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@FunctionalInterface
+interface Function<T, R> {
+    R apply(T t);
+}
+
+public class MyCli {
+    private final Options options;
+    private final CommandLineParser parser;
+    private CommandLine line;
+    private final Map<Option, Function<Object[], Object>> functionMap;
+
+    private MyCli() {
+        options = new Options();
+        parser = new DefaultParser();
+        functionMap = new HashMap<>();
+    }
+
+    private static final MyCli instance = new MyCli();
+
+    public static MyCli getInstance() {
+        return instance;
+    }
+
+    void addOption(Option option, Function<Object[], Object> callback) {
+        options.addOption(option);
+        functionMap.put(option, callback);
+    }
+
+    void parseCommandLine(String[] args) throws ParseException {
+        line = parser.parse(options, args);
+        functionMap.forEach((opt, callback) -> {
+            if (line.hasOption(opt.getOpt())) {
+                String[] values = line.getOptionValues(opt);
+                callback.apply(values);
+            }
+        });
+    }
+
+}
