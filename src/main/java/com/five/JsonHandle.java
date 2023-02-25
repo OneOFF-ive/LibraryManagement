cce588da0f1f9a9ed75bc60943ab4aed0f964957
Wu <2321141691@qq.com>
2023-02-25 21:57:06 +0800

    no message

--- src/main/java/com/five/JsonHandle.java
+++ src/main/java/com/five/JsonHandle.java
@@ -1,4 +1,39 @@
 package com.five;
 
-public class JsonHandle {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonHandle<T> {
+    private final File jsonFile;
+    private List<T> dataList;
+
+    public JsonHandle(String filePath) {
+        jsonFile = new File(filePath);
+    }
+
+    public void readData() throws IOException {
+        if (jsonFile.exists() || jsonFile.createNewFile()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            dataList = objectMapper.readValue(jsonFile, new TypeReference<List<T>>() {
+            });
+        }
+    }
+
+    public void saveData() throws IOException {
+        if (jsonFile.exists() || jsonFile.createNewFile()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            FileOutputStream fileOutputStream = new FileOutputStream(jsonFile, false);
+            objectMapper.writeValue(fileOutputStream, dataList);
+            fileOutputStream.close();
+        }
+    }
+
+    public List<T> getDataList() {
+        return dataList;
+    }
 }
